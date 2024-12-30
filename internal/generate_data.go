0d4a8af32f4651c6284e7588a9943f132b2e4ef9
junho100 <bemodesty306@naver.com>
2024-12-31 00:13:37 +0900

    Feat: add generate dataset

--- internal/generate_data.go
+++ internal/generate_data.go
@@ -1,9 +1,14 @@
 package internal
 
 import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
 	"log"
 	"os"
 	"slices"
+
+	"math/rand"
 )
 
 func GenerateData() {
@@ -18,5 +23,24 @@ func GenerateData() {
 		if err := os.RemoveAll("./test"); err != nil {
 			log.Fatal(err)
 		}
+	}
+
+	if err := os.Mkdir("test", 0750); err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		filePath := fmt.Sprintf("./test/file_%s.csv", fmt.Sprint(i))
+		file, err := os.Create(filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		wr := csv.NewWriter(bufio.NewWriter(file))
+
+		for j := 0; j < 1000; j++ {
+			wr.Write([]string{fmt.Sprint(rand.Intn(999) + 1)})
+		}
+		wr.Flush()
 	}
 }
